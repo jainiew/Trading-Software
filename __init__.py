10ba6b81903dbc7754337952014db20c3ba0e0fe
lainiew <elaine.wu.ew@gmail.com>
2021-04-13 05:45:28 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 # api key for alphavantage
-alphavantage_api_key = "GENWQYVH4N97XK5U"
-finnhub_api_key = "c1prs2qad3icph06qs3g"
+alphavantage_api_key = "key1"
+finnhub_api_key = "key2"
 
 
 ticker = ''
 interval = ''
-api_key = ''
+api_key = alphavantage_api_key # finnhub_api_key
 request = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval={interval}min&apikey={alphavantage_api_key}"
